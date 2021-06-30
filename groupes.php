a9f2760f8f214957084c82a33a81a19e73574acd
Honoré Rasamoelina <10617191+Honomatopic@users.noreply.github.com>
2021-06-30 11:50:03 +0200

    Premier commit
    Premier commit

---
+++ groupes.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link href="css/style.css" rel="stylesheet" type="text/css">
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <title>My Church Library</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">My Library Church</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarColor01">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Accueil
+            <span class="visually-hidden">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Livres</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Echanges</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="groupes.php">Groupes</a>
+        </li>
+
+      </ul>
+      <form class="d-flex"action="<?php $_SERVER["PHP_SELF"];?>">
+                        <input class="form-control me-sm-2" type="text" name="recherche" placeholder="Rechercher">
+                        <input type="submit" class="btn btn-secondary my-2 my-sm-0" name="rechercher" value="Rechercher">
+                    </form>
+                </div>
+            </div>
+            <?php if(isset($_POST["rechercher"])) { 
+                if(isset($_POST["recherche"])) {
+                    echo "Bonjour ". $_POST["recherche"];
+                }
+            };?>
+    </div>
+  </div>
+</nav>
+        <div class="container">
+            <h1>My Church Library</h1>
+            <a href="livres.php" div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="..." class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Christian group</h5>
+                            <p class="card-text">Ceci est un groupe chrétien.</p>
+                            <p class="card-text"><small class="text-muted">Veuillez patientez...</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div></a>
+        </div>
+    </body>
+</html>
---
+++ livres.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link href="css/style.css" rel="stylesheet" type="text/css">
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+        <title>My Church Library</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">My Library Church</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarColor01">
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Accueil
+                                <span class="visually-hidden">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Livres</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Echanges</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="groupes.php">Groupes</a>
+                        </li>
+
+                    </ul>
+                    <form class="d-flex"action="<?php $_SERVER["PHP_SELF"];?>">
+                        <input class="form-control me-sm-2" type="text" name="recherche" placeholder="Rechercher">
+                         <input type="submit" class="btn btn-secondary my-2 my-sm-0" name="rechercher" value="Rechercher">
+                    </form>
+                </div>
+            </div>
+            <?php if(isset($_POST["rechercher"])) { 
+                if(isset($_POST["recherche"])) {
+                    echo "Bonjour ". $_POST["recherche"];
+                }
+            };?>
+        </nav>
+        <h1>My Church Library</h1>
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </body>
+</html>
